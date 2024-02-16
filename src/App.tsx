79423647b69c2fd0528c68480195aa3259923c6f
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-02-16 17:16:10 +0900

    :art: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+
+function App() {
+
+  const initialRows = [
+    {
+      id: 1,
+      name: 'Damien',
+      age: 25,
+      isAdmin: true,
+      country: 'Spain',
+      discount: '',
+    },
+    {
+      id: 2,
+      name: 'Nicolas',
+      age: 36,
+      isAdmin: false,
+      country: 'France',
+      discount: '',
+    },
+    {
+      id: 3,
+      name: 'Kate',
+      age: 19,
+      isAdmin: false,
+      country: 'Brazil',
+      discount: 'junior',
+    },
+  ];
+  const columns: GridColDef[] = [
+      { field: 'name', type: 'string', editable: true },
+      { field: 'age', type: 'number', editable: true },
+      { field: 'isAdmin', type: 'boolean', width: 120, editable: true },
+      {
+        field: 'country',
+        type: 'singleSelect',
+        width: 120,
+        valueOptions: [
+          'Bulgaria',
+          'Netherlands',
+          'France',
+          'United Kingdom',
+          'Spain',
+          'Brazil',
+        ],
+        editable: true
+      },
+  ];
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <DataGrid style={{ height: 300, width: '640px', }} rows={initialRows} columns={columns}
+          onCellKeyDown={(params) => {
+            console.log(params)
+            console.log(params.value)
+            console.log(typeof params.value)
+          }} />
+    </div>
+  )
+}
+
+export default App
